2c059927447158b95d56bb6ff8575d63d09477ac
Jakob Wittmann <WittmannJakob09402@googlemail.com>
2018-10-16 14:08:25 +0200

    some logic for options menu

--- app/src/main/java/com/stell/wowa/bytepluto/MainActivity.java
+++ app/src/main/java/com/stell/wowa/bytepluto/MainActivity.java
@@ -1,13 +1,52 @@
 package com.stell.wowa.bytepluto;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.mainMenuPost:
+                Log.d(TAG, "Post was pressed");
+                return true;
+
+            case R.id.mainMenuHelp:
+                Log.d(TAG, "Help was pressed");
+                return true;
+
+            case R.id.mainMenuTest:
+                Log.d(TAG, "Test was pressed");
+                return true;
+
+            case R.id.mainMenuManageAccount:
+                Log.d(TAG, "ManageAccount was pressed.");
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
