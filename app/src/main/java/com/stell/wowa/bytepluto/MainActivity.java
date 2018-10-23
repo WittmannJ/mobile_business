42aca46fad203a6332052f5e31efb856695da8f1
wowa <waldemarstell@gmx.de>
2018-10-23 14:06:41 +0200

    Added Logs for the lifecycles of the app

--- app/src/main/java/com/stell/wowa/bytepluto/MainActivity.java
+++ app/src/main/java/com/stell/wowa/bytepluto/MainActivity.java
@@ -16,9 +16,45 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Log.d(TAG, "my first message!");
+        Log.d(TAG, "onCreate");
         setContentView(R.layout.activity_main);
         Toast.makeText(getApplicationContext(), R.string.dashboard_Text, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "onResume");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(TAG, "onStop");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(TAG, "onPause");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(TAG, "onDestroy");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(TAG, "onReStart");
     }
 
     @Override
