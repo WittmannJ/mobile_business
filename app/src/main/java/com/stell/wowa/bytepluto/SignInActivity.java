e7e641d8b224dd35e4e3759987f98c73d7201b60
Jakob Wittmann <WittmannJakob09402@googlemail.com>
2018-10-30 11:17:55 +0100

    added signin logic to signin activity with firebase

--- app/src/main/java/com/stell/wowa/bytepluto/SignInActivity.java
+++ app/src/main/java/com/stell/wowa/bytepluto/SignInActivity.java
@@ -2,15 +2,28 @@ package com.stell.wowa.bytepluto;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignInActivity extends AppCompatActivity implements View.OnClickListener{
 
     EditText mEditTextEmail;
     EditText mEditTextPassword;
+
+    private FirebaseAuth mAuth;
+
+    private static String TAG = "SignIn Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +32,10 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
 
         mEditTextEmail = (EditText) findViewById(R.id.signInEmail);
         mEditTextPassword = (EditText) findViewById(R.id.signInPassword);
+
+        mAuth = FirebaseAuth.getInstance();
+
+
 
         ((Button) findViewById(R.id.signInButtonSignIn)).setOnClickListener(this);
         ((Button) findViewById(R.id.signInButtonResetPassword)).setOnClickListener(this);
@@ -58,10 +75,35 @@ public class SignInActivity extends AppCompatActivity implements View.OnClickLis
     }
 
     private void doSignIn() {
+            String mail = mEditTextEmail.getText().toString();
+            String password = mEditTextPassword.getText().toString();
 
-        Intent intent = new Intent(getApplication(), MainActivity.class);
-        intent.putExtra("message", "hi");
-        startActivityForResult(intent, 1);
+            mAuth.signInWithEmailAndPassword(mail, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "signInUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+                                Toast.makeText(getApplicationContext(), "User signed in.",
+                                        Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplication(), MainActivity.class);
+                                intent.putExtra("message", "hi");
+                                startActivityForResult(intent, 1);
+
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "signInUserWithEmail:failure", task.getException());
+                                Toast.makeText(getApplicationContext(), "User signIn failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            // ...
+                        }
+                    });
+
+
+
 
     }
 }
