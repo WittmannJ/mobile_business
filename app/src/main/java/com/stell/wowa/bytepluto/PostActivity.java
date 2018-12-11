3ecb2b775339513b85bb2c394801c30d4406422d
Jakob Wittmann <WittmannJakob09402@googlemail.com>
2018-12-11 13:21:12 +0100

    send posts to firebase realtime db is now possible

--- app/src/main/java/com/stell/wowa/bytepluto/PostActivity.java
+++ app/src/main/java/com/stell/wowa/bytepluto/PostActivity.java
@@ -1,16 +1,29 @@
 package com.stell.wowa.bytepluto;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ServerValue;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
     EditText mPostTitle;
     EditText mPostBody;
+    FirebaseAuth mAuth;
+    FirebaseUser mCurrentUser;
+    FirebaseDatabase mDatabase;
+    DatabaseReference myRef;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +34,11 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
         mPostBody  = (EditText) findViewById( R.id.postText);
 
         ((Button) findViewById( R.id.postButtonPost)).setOnClickListener( this );
+
+        mAuth = FirebaseAuth.getInstance();
+        mCurrentUser = mAuth.getCurrentUser();
+
+
 
         //  TODO: Remove; test setting
 
@@ -41,8 +59,27 @@ public class PostActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     private void doPost() {
+
+        // TODO: Add checkings before posting
+        Map<String, Object> postMap = new HashMap<>();
+        postMap.put("uid", mCurrentUser.getUid());
+        postMap.put("author", mCurrentUser.getEmail());
+        postMap.put("title", mPostTitle.getText().toString());
+        postMap.put("body", mPostBody.getText().toString());
+        postMap.put("timestamp", ServerValue.TIMESTAMP);
+
+        mDatabase = FirebaseDatabase.getInstance();
+        myRef = mDatabase.getReference("Posts/");
+        myRef.push().setValue(postMap);
+
+
+
+
+
         Intent intent = new Intent(getApplication(),
                 MainActivity.class);
         startActivity(intent);
+
+
     }
 }
